e2f96f3295e0a88c0c9ef5aacd09c127a68894be
Kyle Merklein <kyle.merklein@gmail.com>
2014-04-03 15:01:01 -0500

    Initialize repository with spree storefront

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyStore::Application.config.secret_key_base = '57066351c7632dbc4b4294c44026cf01d3942c8540d326c4eef992d026c81fed158316a59282213610e75d0d3a27e4843de840ac9c09d6b81fd79526545b16e5'
